e93ce6f1be617670ddee1e3d362f63f6c8bd8801
Marchenko <aleks.marchenko802@gmail.com>
2024-07-19 22:26:18 +0300

    first commit

---
+++ src/main/java/ua/oleksandr/useraggregator/UserAggregatorApplication.java
@@ -0,0 +1,13 @@
+package ua.oleksandr.useraggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAggregatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserAggregatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/oleksandr/useraggregator/UserAggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.oleksandr.useraggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAggregatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
